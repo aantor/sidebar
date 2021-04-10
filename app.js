1b323f2f5f56562d299b04df5625222b94eb71bb
aantor <antorinfo@gmail.com>
2021-04-10 15:21:47 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const sidebar = document.querySelector('.sidebar');
+const sidebarToggle = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+
+sidebarToggle.addEventListener('click', () => {
+  sidebar.classList.toggle('show-sidebar');
+  console.log('object');
+});
+
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
+});
